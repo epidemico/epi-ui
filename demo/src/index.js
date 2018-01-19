951af9514af3683d94a3dc3a6b20101187fb0f90
Harold Rodriguez <harold@planetharold.com>
2018-01-19 14:26:29 -0500

    add author links

--- demo/src/index.js
+++ demo/src/index.js
@@ -14,6 +14,23 @@ if (document) {
 const EpiUIBrowser = () => (
   <div className="demo">
     <h2>{title}</h2>
+    <p>
+      React components for data visualization. Available on{' '}
+      <a href="https://github.com/epidemico/epi-ui" target="_blank" rel="noopener">
+        GitHub
+      </a>. Made with ❤️ by{' '}
+      <a href="https://www.epidemico.com" target="_blank" rel="noopener">
+        Epidemico
+      </a>.
+    </p>
+    <a href="https://github.com/epidemico/epi-ui" target="_blank" rel="noopener">
+      <img
+        className="github"
+        src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67"
+        alt="Fork me on GitHub"
+        data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
+      />
+    </a>
     <ul className="tag-group-list" style={{ listStyleType: 'none', margin: 0, padding: 0 }}>
       {hasDemoData.map(key => (
         <li className="tag" key={key}>
