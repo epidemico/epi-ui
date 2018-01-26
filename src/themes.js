6cbead5d980cb51dde7c8516f7d0039b19aae4c7
Harold Rodriguez <harold@planetharold.com>
2018-01-26 12:05:10 -0500

    add themes

---
+++ src/themes.js
@@ -0,0 +1,25 @@
+// Define themes:
+const themes = {
+  default: {
+    // the "default" theme
+    colors: [
+      '#32abe3',
+      '#e67e22',
+      '#73bf67',
+      '#fc5454',
+      '#b26fd4',
+      '#937443',
+      '#eb95da',
+      '#8a8989',
+      '#66cdcc',
+      '#2c3e50',
+    ],
+  },
+}
+
+// Export default theme fields.
+const { colors } = themes.default
+export { colors }
+
+// Export all themes as the default module.
+export default themes
