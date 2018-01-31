b420e68110b1e9e7f2d1c3c818ec7e5499ac920b
Harold Rodriguez <harold@planetharold.com>
2018-01-31 17:31:13 -0500

    add ProgressIndicator

---
+++ src/ProgressIndicator.js
@@ -0,0 +1,28 @@
+// @flow
+import React from 'react'
+import styles from './styles/ProgressIndicator.css'
+
+type PropTypes = {
+  currentStep: number,
+  totalSteps: number,
+}
+
+const ProgressIndicator = (props: PropTypes) => {
+  const { currentStep, totalSteps } = props
+  const progressBarWidth = currentStep / totalSteps * 100
+  const widthClass = {
+    width: `${progressBarWidth}%`,
+  }
+  return (
+    <div className="progress-indicator">
+      <div className="progress-label">
+        Step {currentStep} of {totalSteps}
+      </div>
+      <div className="progress-bar-outer">
+        <div className="progress-bar-inner" style={widthClass} />
+      </div>
+    </div>
+  )
+}
+
+export default ProgressIndicator
